acd1fbc81199cc7214312bdb3b4652c12ad4bac2
TheoChrn <theo.chrn@gmail.com>
2024-12-10 22:49:40 +0100

    commit

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,37 +1,61 @@
-import { BrowserRouter, Route, Routes as RouterRoutes } from "react-router";
-import NotFound from "./not-found";
 import Home from "@/routes/home/home";
-import { PagesLayout } from "@/routes/pages/pages-layout";
 import { Characters } from "@/routes/pages/characters/characters";
-import { Movies } from "@/routes/pages/movies/movies";
-import { Vehicles } from "@/routes/pages/vehicles/vehicles";
-import { Starships } from "@/routes/pages/starships/starships";
-import { Planets } from "@/routes/pages/planets/planets";
-import { Species } from "@/routes/pages/species/species";
 import { RootLayout } from "@/routes/root-layout";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import NotFound from "./not-found";
+import { PagesLayout } from "@/routes/pages/pages-layout";
 import { Character } from "@/routes/pages/characters/characterId/character";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        element: <PagesLayout />,
+        children: [
+          {
+            path: "characters",
+            element: <Characters />,
+            // action: newAction(queryClient),
+            errorElement: <NotFound />,
+          },
+          {
+            path: "characters/:characterId",
+            element: <Character />,
+            // loader: contactLoader(queryClient),
+            // action: contactAction(queryClient),
+            errorElement: <NotFound />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <RouterRoutes>
-        <Route element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route element={<PagesLayout />}>
-            <Route path="/characters">
-              <Route index element={<Characters />} />
-              <Route path=":characterId" element={<Character />} />
-            </Route>
-
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/vehicles" element={<Vehicles />} />
-            <Route path="/starships" element={<Starships />} />
-            <Route path="/planets" element={<Planets />} />
-            <Route path="/species" element={<Species />} />
-          </Route>
-        </Route>
-      </RouterRoutes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <RouterRoutes>
+    //     <Route element={<RootLayout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="*" element={<NotFound />} />
+    //       <Route element={<PagesLayout />}>
+    //         <Route path="/characters" element={<Characters />} />
+    //         <Route path="/movies" element={<Movies />} />
+    //         <Route path="/vehicles" element={<Vehicles />} />
+    //         <Route path="/starships" element={<Starships />} />
+    //         <Route path="/planets" element={<Planets />} />
+    //         <Route path="/species" element={<Species />} />
+    //       </Route>
+    //     </Route>
+    //   </RouterRoutes>
+    // </BrowserRouter>
   );
 }
