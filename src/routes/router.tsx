d1ebeab4048ce5bb53df7d7478e6c95d1de80da8
TheoChrn <theo.chrn@gmail.com>
2024-12-11 18:09:36 +0100

    corrected router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -17,7 +17,7 @@ import { RootLayout } from "@/routes/root-layout";
 import { QueryClient } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router"; // Utilisation de BrowserRouter ici
 import NotFound from "./not-found";
 
 const queryClient = new QueryClient({
@@ -28,80 +28,67 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        element: <PagesLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: "*",
-            element: <NotFound />,
-          },
-          {
-            path: "characters",
-            loader: charactersLoader(queryClient),
-            element: (
-              <Suspense
-                fallback={<SubPageLoader>Loading characters</SubPageLoader>}
-              >
-                <Characters />
-              </Suspense>
-            ),
-            errorElement: <ErrorPage />,
-          },
-          {
-            path: "characters/:characterId",
-            element: (
-              <Suspense
-                fallback={<SubPageLoader>Loading character</SubPageLoader>}
-              >
-                <Character />
-              </Suspense>
-            ),
-            loader: characterLoader(queryClient),
-            errorElement: <ErrorPage />,
-          },
-          {
-            path: "films",
-            loader: filmsLoader(queryClient),
-            element: (
-              <Suspense
-                fallback={<SubPageLoader>Loading movies</SubPageLoader>}
-              >
-                <Films />
-              </Suspense>
-            ),
-            errorElement: <ErrorPage />,
-          },
-          {
-            path: "films/:filmId",
-            element: (
-              <Suspense fallback={<SubPageLoader>Loading movie</SubPageLoader>}>
-                <Film />
-              </Suspense>
-            ),
-            loader: filmLoader(queryClient),
-            errorElement: <ErrorPage />,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
 export function Router() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter basename="/FSD57A-react-projet-final">
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route element={<PagesLayout />} errorElement={<ErrorPage />}>
+              <Route path="*" element={<NotFound />} />
+              <Route
+                path="characters"
+                loader={charactersLoader(queryClient)}
+                element={
+                  <Suspense
+                    fallback={<SubPageLoader>Loading characters</SubPageLoader>}
+                  >
+                    <Characters />
+                  </Suspense>
+                }
+                errorElement={<ErrorPage />}
+              />
+              <Route
+                path="characters/:characterId"
+                loader={characterLoader(queryClient)}
+                element={
+                  <Suspense
+                    fallback={<SubPageLoader>Loading character</SubPageLoader>}
+                  >
+                    <Character />
+                  </Suspense>
+                }
+                errorElement={<ErrorPage />}
+              />
+              <Route
+                path="films"
+                loader={filmsLoader(queryClient)}
+                element={
+                  <Suspense
+                    fallback={<SubPageLoader>Loading movies</SubPageLoader>}
+                  >
+                    <Films />
+                  </Suspense>
+                }
+                errorElement={<ErrorPage />}
+              />
+              <Route
+                path="films/:filmId"
+                loader={filmLoader(queryClient)}
+                element={
+                  <Suspense
+                    fallback={<SubPageLoader>Loading movie</SubPageLoader>}
+                  >
+                    <Film />
+                  </Suspense>
+                }
+                errorElement={<ErrorPage />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
