ea49239943d65c02a04971127f0d4a89456413f4
TheoChrn <theo.chrn@gmail.com>
2024-12-11 17:48:51 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production" ? "/FSD57A-react-projet-final/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
